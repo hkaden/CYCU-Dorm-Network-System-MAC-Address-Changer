cac440babae866bc4dc0d752bab055a45e0b140c
l2003201 <davidlui@Macintosh-2.local>
2016-02-18 18:54:39 +0800

    Mac Change Function Done.

--- main.py
+++ main.py
@@ -18,13 +18,18 @@ def loginWeb(site):
     re=opender.open(site,urllib.urlencode(LoginValue))
     print cj
 
-    """
+    ChangeMacValue={
+                'ipAddr':'140.135.29.128',
+                'status':'%A5%BF%B1%60%A8%CF%A5%CE%A4%A4',
+                'macAddr':'44-b8-0a-96-fa-22',
+                'update_mac':'update'
+               }
     #登陆成功之后的带着cookie的页面访问
-    pc='https://console.oray.com'#比如个人中心页面
-    pcre=opender.open(pc)
+    pc='http://ccdna.cycu.edu.tw/register_dorm/activate.php'#比如个人中心页面
+    pcre=opender.open(pc,urllib.urlencode(ChangeMacValue))
     print pcre.getcode()
     print pcre.read()
-    """
+
 
 
 if __name__ == '__main__':
